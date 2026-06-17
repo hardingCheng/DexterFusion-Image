b4d1f4296a5ac2c2ad5ea4f064020bd23ab05b8e
dexter <hardingcheng@163.com>
2026-06-17 11:57:30 +0800

    feat: 更新参考图大小

--- nb-app/src/config/upload.ts
+++ nb-app/src/config/upload.ts
@@ -2,8 +2,8 @@ import { isGptImage2Model } from './models';
 
 export const MAX_REFERENCE_IMAGES = 9;
 export const MAX_GPT_IMAGE_2_REFERENCE_IMAGES = 6;
-export const MAX_REFERENCE_IMAGE_BYTES = 10 * 1024 * 1024;
-export const MAX_REFERENCE_IMAGE_SIZE_LABEL = '10MB';
+export const MAX_REFERENCE_IMAGE_BYTES = 15 * 1024 * 1024;
+export const MAX_REFERENCE_IMAGE_SIZE_LABEL = '15MB';
 
 export const getMaxReferenceImages = (modelName?: string) =>
   isGptImage2Model(modelName) ? MAX_GPT_IMAGE_2_REFERENCE_IMAGES : MAX_REFERENCE_IMAGES;
